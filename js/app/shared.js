8ebb19cbb99fbcb3c7fdfb31915bab92e31a13d5
ya <ya@dei.su>
2019-01-05 14:51:54 +0300

    Remove leasing

--- js/app/shared.js
+++ js/app/shared.js
@@ -4574,7 +4574,6 @@
 		ctrl.hasRecipient = hasRecipient;
 		ctrl.addressCopied = addressCopied;
 		ctrl.isLeasing = isLeasing;
-		ctrl.cancelLeasing = cancelLeasing;
 
 		function addressCopied () {
 			return notificationService.notice('Address has been copied');
@@ -4594,12 +4593,6 @@
 
 		function isLeasing () {
 			return ctrl.transaction.type === constants.START_LEASING_TRANSACTION_TYPE;
-		}
-
-		function cancelLeasing () {
-			$scope.$emit(events.LEASING_CANCEL, {
-				startLeasingTransaction: ctrl.transaction
-			});
 		}
 
 		function fullTransactionData () {
@@ -4640,69 +4633,14 @@
 	var FEE_CURRENCY = Currency.BASE;
 	var DEFAULT_ERROR_MESSAGE = 'The Internet connection is lost';
 
-	// TODO : add the `exceptField` attribute or a list of all the needed fields.
-
-	function WavesTransactionHistoryController($scope, events, constants, applicationContext, apiService, leasingRequestService, notificationService, dialogService) {
+	function WavesTransactionHistoryController() {
 		var ctrl = this;
-		var minimumFee = new Money(constants.MINIMUM_TRANSACTION_FEE, FEE_CURRENCY);
+		var minimumFee = new Money(Constants.MINIMUM_TRANSACTION_FEE, FEE_CURRENCY);
 
-		ctrl.cancelLeasing = cancelLeasing;
 		ctrl.confirm = {
 			fee: minimumFee
 		};
-
-		$scope.$on(events.LEASING_CANCEL, function (event, eventData) {
-			ctrl.startLeasingTransaction = eventData.startLeasingTransaction;
-
-			ctrl.confirm.recipient = ctrl.startLeasingTransaction.recipient;
-			ctrl.confirm.amount = ctrl.startLeasingTransaction.formatted.amount;
-			ctrl.confirm.asset = ctrl.startLeasingTransaction.formatted.asset;
-
-			dialogService.open('#cancel-leasing-confirmation');
-		});
-
-		function cancelLeasing () {
-			var cancelLeasing = {
-				startLeasingTransactionId: ctrl.startLeasingTransaction.id,
-				fee: minimumFee
-			};
-
-			var sender = {
-				publicKey: applicationContext.account.keyPair.public,
-				privateKey: applicationContext.account.keyPair.private
-			};
-
-			var transaction = leasingRequestService.buildCancelLeasingRequest(cancelLeasing, sender);
-
-			apiService.leasing.cancel(transaction)
-				.then(function () {
-					notificationService.notice('Leasing transaction of ' +
-						ctrl.startLeasingTransaction.formatted.amount + ' ' +
-						ctrl.startLeasingTransaction.formatted.asset + ' has been cancelled.');
-				})
-				.catch(function (exception) {
-					if (exception) {
-						if (exception.data) {
-							notificationService.error(exception.data.message);
-						} else if (exception.message) {
-							notificationService.error(exception.message);
-						} else if (exception.statusText) {
-							notificationService.error(exception.statusText);
-						} else {
-							notificationService.error(DEFAULT_ERROR_MESSAGE);
-						}
-					} else {
-						notificationService.error(DEFAULT_ERROR_MESSAGE);
-					}
-
-					dialogService.close();
-				});
-
-			return true;
-		}
 	}
-
-	WavesTransactionHistoryController.$inject = ['$scope', 'ui.events', 'constants.ui', 'applicationContext', 'apiService', 'leasingRequestService', 'notificationService', 'dialogService'];
 
 	angular
 		.module('app.shared')
