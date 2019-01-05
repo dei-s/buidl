10abbc74e43622657c7a13e7ddfc4bea4abe83be
ya <ya@dei.su>
2019-01-06 01:30:09 +0300

    Updated core.storage

--- js/core/storage.js
+++ js/core/storage.js
@@ -5,12 +5,6 @@ var Storage = (function(){
 
 	var storage;
 
-	if (isMir()) {
-		var $key = 'MirAccounts';
-	} else {
-		var $key = 'WavesAccounts';
-	}
-
 	if (angular.isUndefined(Constants.NETWORK_NAME))
 		throw new Error('Network name hasn\'t been configured');
 
@@ -23,7 +17,7 @@ var Storage = (function(){
 	function clear() {
 		return new Promise(function(resolve, reject){
 			window.localStorage.removeItem($key);
-			resolve(); //return $q.when();
+			resolve();
 		});
 	}
 
@@ -90,7 +84,7 @@ var Storage = (function(){
 			if (serialized) {
 				data = angular.fromJson(serialized);
 			}
-			resolve(data); //return $q.when(data);
+			resolve(data);
 		});
 	}
 
@@ -137,147 +131,4 @@ var Storage = (function(){
 		loadState: loadState,
 		saveState: saveState
 	}
-})();
-
-(function() {
-	'use strict';
-
-	angular
-		.module('waves.core.services')
-		.service('chromeStorageService', ['$q', function ($q) {
-			var $key = Storage.$key;
-			var self = this;
-
-			self.saveState = function (state) {
-				var deferred = $q.defer();
-				var json = {};
-				json[$key] = state;
-
-				chrome.storage.local.set(json, function () {
-					deferred.resolve();
-				});
-
-				return deferred.promise;
-			};
-
-			self.loadState = function () {
-				var deferred = $q.defer();
-
-				self.loadSyncState().then(function (syncState) {
-					if (syncState) {
-						self.saveState(syncState)
-							.then(function () {
-								return self.clearSyncState();
-							})
-							.then(function () {
-								deferred.resolve(syncState);
-							});
-					} else {
-						chrome.storage.local.get($key, function (data) {
-							deferred.resolve(data[$key]);
-						});
-					}
-				});
-
-				return deferred.promise;
-			};
-
-			self.loadSyncState = function () {
-				var deferred = $q.defer();
-
-				chrome.storage.sync.get($key, function (data) {
-					deferred.resolve(data[$key]);
-				});
-
-				return deferred.promise;
-			};
-
-			self.clearSyncState = function () {
-				var deferred = $q.defer();
-
-				chrome.storage.sync.clear(function () {
-					deferred.resolve();
-				});
-
-				return deferred.promise;
-			};
-		}]);
-})();
-
-(function() {
-	'use strict';
-
-	angular
-		.module('waves.core.services')
-		.service('html5StorageService', ['constants.network', '$window', '$q', function(constants, window, $q) {
-			if (angular.isUndefined(constants.NETWORK_NAME))
-				throw new Error('Network name hasn\'t been configured');
-
-			if (isMir()) {
-				var $key = 'Mir' + constants.NETWORK_NAME;
-			} else {
-				var $key = 'Waves' + constants.NETWORK_NAME;
-			}
-
-			this.saveState = function(state) {
-				var serialized = angular.toJson(state);
-				window.localStorage.setItem($key, serialized);
-				return $q.when();
-			};
-
-			this.loadState = function() {
-				var data;
-				var serialized = window.localStorage.getItem($key);
-
-				if (serialized) {
-					data = angular.fromJson(serialized);
-				}
-
-				return $q.when(data);
-			};
-
-			this.clear = function() {
-				window.localStorage.removeItem($key);
-
-				return $q.when();
-			};
-		}]);
-})();
-
-(function() {
-	'use strict';
-
-	var STORAGE_STRUCTURE_VERSION = 1;
-
-	angular
-		.module('waves.core.services')
-		.provider('storageService', [function () {
-			function getStorageVersion () {
-				return STORAGE_STRUCTURE_VERSION;
-			}
-
-			function isLocalStorageEnabled(window) {
-				var storage, fail, uid;
-				try {
-					uid = String(new Date());
-					(storage = window.localStorage).setItem(uid, uid);
-					fail = storage.getItem(uid) != uid;
-					if (!fail)
-						storage.removeItem(uid);
-					else
-						storage = false;
-				}
-				catch (exception) {
-				}
-				return storage;
-			}
-
-			this.$get = ['$window', 'chromeStorageService', 'html5StorageService',
-				function($window, chromeStorageService, html5StorageService) {
-					var result = isLocalStorageEnabled($window) ? html5StorageService : chromeStorageService;
-					result.getStorageVersion = getStorageVersion;
-
-					return result;
-				}];
-		}]);
 })();
