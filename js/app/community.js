0648cf7b7fedd19b179b01db89b9ffa504a04fa1
ya <ya@dei.su>
2019-01-07 00:30:51 +0300

    Added app.community

---
+++ js/app/community.js
@@ -0,0 +1,142 @@
+var MSG_NODE_ADDRESS = DEI_NODE_ADDRESS;
+if (isMir()) {
+	var MSG_BASE_ID = 'MIR';
+	var MSG_AMOUNT_MIN = 100000; // 100000 = 0.001, 100000000 = 1
+	var MSG_ASSET_ID = '';
+	var MSG_WALLET_ADDR = '3MSpWtFmVwob2GHbMk4qb2dJXEnHykYYLBR';
+} else {
+	var MSG_BASE_ID = 'WAVES';
+	var MSG_AMOUNT_MIN = 100000; // 0.001 МИР
+	var MSG_ASSET_ID = 'HdPJha3Ekn1RUR2K9RrY7SG9xK1b21AHPwkL8pcwTmSZ'; // МИР
+	var MSG_WALLET_ADDR = '3PEPpJoJNqrU6T6AAdXB3yMzUseEaUPniCw'; // DEI
+}
+
+var communityApp = new Vue({
+	el: '#mBB-community',
+	data: {
+		project: {
+			name: ""
+		},
+		message: '',
+		pinnedMessage: {},
+		messages: [],
+		wall: MSG_WALLET_ADDR,
+		node: MSG_NODE_ADDRESS,
+		amount: '0.001',
+		fee: '0.001',
+		minAmount: MSG_AMOUNT_MIN,
+		pinAmount: 1000000,
+		last: 0
+	},
+	created: function() {
+		this.update();
+	},
+	methods: {
+		run: function() {
+			this.start();
+		},
+		start: function() {
+			setInterval(this.update, 10000);
+		},
+		formatAmount: function(amount) {
+			return (amount / 100000000).toLocaleString('ru-ru', {maximumSignificantDigits: 20}) + 'МИР';
+		},
+		formatSender: function(sender) {
+			return '...' + sender.slice(-7);
+		},
+		formatTime: function(timestamp) {
+			let time = new Date(timestamp);
+			return time.toLocaleString();
+		},
+		loadPosts: async function(limit) {
+			let response = await axios.get(this.node + '/transactions/address/' + this.wall + '/limit/' + limit);
+			return response.data;
+		},
+		findByKey: function(array, id) {
+			let element = array.find(element => {
+				return element.key == id ? element : false;
+			});
+			return element;
+		},
+		getName: async function(sender) {
+			try {
+				let response = await axios.get(this.node + '/addresses/data/' + sender + '/waves-wall-name');
+				return response.data ? response.data : '';
+			} catch(err) {
+				console.log(err);
+				return '';
+			}
+		},
+		update: async function() {
+			let lastTx = await this.loadPosts(1);
+			let lastTime = lastTx[0][0].timestamp;
+			if (lastTime != this.last) {
+				this.last = lastTime.valueOf();
+				let data = await this.loadPosts(10000);
+				this.messages = [];
+				await data[0].forEach(item => {
+					if (item.attachment && item.amount >= this.minAmount /*&& item.assetId == MSG_ASSET_ID*/) {
+						let msg = this.decode(item.attachment);
+
+						this.messages.unshift({
+							sender: item.sender,
+							text: msg,
+							time: item.timestamp,
+							amount: item.amount,
+							id: item.id
+						});
+						let pinned = this.messages.find(element => {
+							return element.amount > this.pinAmount ? element : false;
+						});
+						this.pinnedMessage = pinned ? pinned : '';
+					}
+				});
+				this.$refs.msgWrapper.scrollTop = this.$refs.msgWrapper.scrollHeight;
+			}
+		},
+		findMax: function(arr) {
+			return arr.reduce(function(prev, curr){
+				return prev.amount >= curr.amount ? prev : curr;
+			});
+		},
+		decode: function(text) {
+			let bytes = Base58.decode(text);
+			let str = '';
+			for (let i = 0; i < bytes.length; i++) {
+				str += String.fromCharCode(bytes[i]);
+			}
+			return decodeURIComponent(escape(str));
+		},
+		send: async function() {
+			let msg = this.message;
+			let params = {
+				type: 4,
+				data: {
+					amount: {
+						assetId: MSG_ASSET_ID,
+						tokens: this.amount
+					},
+					fee: {
+						assetId: MSG_BASE_ID,
+						tokens: this.fee
+					},
+					recipient: this.wall,
+					attachment: msg
+				}
+			}
+			if (this.checkKeeper()) {
+				try {
+					let res = await window.Waves.signAndPublishTransaction(params);
+					this.message = '';
+				} catch (err) {
+					alert(err.message);
+				}
+			} else {
+				alert('Please, install Waves Keeper.\nFollow the link at the bottom of the page.');
+			}
+		},
+		checkKeeper: function() {
+			return typeof window.Waves !== 'undefined';
+		}
+	}
+});
