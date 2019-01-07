e93e849941e0fd3a6d60bd54758fd547a6988e9d
ya <ya@dei.su>
2019-01-07 05:51:33 +0300

    Updated core.constants

--- js/core/constants.js
+++ js/core/constants.js
@@ -29,6 +29,33 @@ var Constants = (function(){
 		JAVA_MAX_LONG: 9223372036854775807,
 		MAXIMUM_ATTACHMENT_BYTE_SIZE: 140,
 
+		/* Economic Protocol - Economic Message Transfer Protocol
+		Example:
+		{v:1,mt:1,of:1,to:2}
+		v - version (= 1)
+		mt - message type:
+			1=Offer - выставление оферты
+			2=OfferAccept - принять оферту (= заключить договор)
+			3=OfferReject - отклонить оферту
+			4=OfferWithdraw - отозвать оферту
+			5=Voting - запрос на голосование
+			6=Vote - голос
+		of - от кого (идентификатор)
+		to - кому (идентификатор)
+		tr - ID предыдущей транзакции в контексте текущего сообщения (например по какому голосованию отдаётся голос или на какую оферту производится акцепт)
+		add - addition - дополнение, пояснение
+
+		Vote example:
+		{v:1,mt:6,tr:2evmK6eYHC3HdFjowBnYsYaUj9ZJz1ssGryKWshToxJX,vote:yes,add:"Принять участника petrenko_ivan13 в DEI"}
+		{v:1,mt:6,tr:2evmK6eYHC3HdFjowBnYsYaUj9ZJz1ssGryKWshToxJX,vote:yes,add:"vk.com/petrenko_ivan13 3PJ7J7SRCh3yYfHBvQAPCtZSFeThUL6gNRV"}
+		*/
+		MSG_TYPE_OFFER: 1,
+		MSG_TYPE_OFFER_ACCEPT: 2,
+		MSG_TYPE_OFFER_REJECT: 3,
+		MSG_TYPE_OFFER_WITHDRAW: 4,
+		MSG_TYPE_VOTING: 5,
+		MSG_TYPE_VOTE: 6,
+
 		/* transactions */
 		PAYMENT_TRANSACTION_TYPE : 2,
 		ASSET_ISSUE_TRANSACTION_TYPE: 3,
@@ -36,8 +63,8 @@ var Constants = (function(){
 		ASSET_REISSUE_TRANSACTION_TYPE: 5,
 		ASSET_BURN_TRANSACTION_TYPE: 6,
 		EXCHANGE_TRANSACTION_TYPE: 7,
-		START_LEASING_TRANSACTION_TYPE: 8,
-		CANCEL_LEASING_TRANSACTION_TYPE: 9,
+		START_LEASING_TRANSACTION_TYPE: 8, // No use
+		CANCEL_LEASING_TRANSACTION_TYPE: 9, // No use
 		CREATE_ALIAS_TRANSACTION_TYPE: 10,
 		MASS_PAYMENT_TRANSACTION_TYPE: 11
 	}
