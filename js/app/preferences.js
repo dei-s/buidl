f5c9bf8c469d4657155f995a040ed8c022dd6b93
ya <ya@dei.su>
2019-01-09 20:07:57 +0300

    Added preferences

---
+++ js/app/preferences.js
@@ -0,0 +1,41 @@
+var preferencesApp = (function(){
+	var isDevMode = false;
+	var list = [];
+
+	function add(eid) {
+		var e = $(eid);
+		if (!e) return;
+		list.push({
+			eid: eid,
+			displayModeDefault: getCssDisplay('#messaginTabIcon')
+		});
+	}
+
+	function getCssDisplay(eid) {
+		var e = $(eid);
+		if (!e) return;
+		return e.css("display");
+	}
+
+	function toggleDevMode() {
+		isDevMode = !isDevMode;
+		if (isDevMode) {
+			list.forEach(function(item){
+				$(item.eid).show();
+			});
+		} else {
+			list.forEach(function(item){
+				$(item.eid).css("display", item.displayModeDefault);
+			});
+		}
+	}
+
+	add('#blocksTabIcon');
+	add('#communityTabIcon');
+	add('#exchangeTabIcon');
+	add('#messaginTabIcon');
+
+	return {
+		toggleDevMode: toggleDevMode
+	}
+})();
